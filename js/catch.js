de0098dea8888bebbea3e6095f4ba4db1c88872c
Kajakaletakifajny <wyslijtokajaki@gmail.com>
2022-02-03 09:27:19 +0100

    a

---
+++ js/catch.js
@@ -0,0 +1,15 @@
+$(function(){
+    //najpierw co zmienić, potem jak zmienić
+    $('#page').css('background-color','#00ffff');
+    //jak wskazać obiekt przez id
+
+    $('h2').css('letter-spacing','5px');
+    //jak wskazać obiekt przez znacznik
+
+    $('.link').css({'color': 'black','text-decoration': 'none'});
+    //jak wskazać obiekt przez klase
+
+    $('[href="#"]').attr('href','https://pl.wikipedia.org/wiki/J%C4%99zyk_programowania');
+    //jak wskazać obiekt przez wartość własności (attribute)
+
+});
