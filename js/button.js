3c1ff48ca3e93f822cf083d0655e2aa28d0b2b57
Kajakaletakifajny <wyslijtokajaki@gmail.com>
2022-02-11 13:54:14 +0100

    a

--- js/button.js
+++ js/button.js
@@ -1,3 +1,9 @@
-$(function () {
+/*$(function kot() { //to też nie działa
     $('ul').append('C++');
-});
+});*/
+
+function kot()
+{
+    alert("pies");
+    //$('ul').append('C++');//gdy się to odkomentuje to nie działa
+}
