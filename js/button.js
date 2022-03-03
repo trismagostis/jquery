96e945b187cfadf07ca2506770d80e7f1ca7419a
Just-iiiiiiiiiiiiiin <wyslijtokajaki@gmail.com>
2022-03-03 09:34:38 +0100

    a

--- js/button.js
+++ js/button.js
@@ -1,9 +1,9 @@
-/*$(function kot() { //to też nie działa
-    $('ul').append('C++');
-});*/
+function kot() {
+    $('ul').append('<li> C++ </li>');
+};
 
-function kot()
+/*function kot()
 {
     alert("pies");
     //$('ul').append('C++');//gdy się to odkomentuje to nie działa
-}
+}*/
